033095dea4eb0da9b186e916c4275da845926034
apthtc1 <apthtc78@gmail.com>
2026-07-25 18:18:15 +0300

    Update knowledge base 2026-07-25 18:18

--- publish.ps1
+++ publish.ps1
@@ -1,15 +1,22 @@
-$ErrorActionPreference = "Stop"
+﻿$ErrorActionPreference = "Stop"
 
 $VAULT_PATH = "C:\Users\aptht\Desktop\Новая папка\Obsidian NAS\G"
 $SITE_PATH = "$env:USERPROFILE\quartz-site"
 
 Write-Host "Syncing notes from Obsidian..."
 
-# Очищаем content перед копированием
+# Очищаем content
 Remove-Item -Path "$SITE_PATH\content\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 # Копируем все файлы и папки из vault (кроме .obsidian и .trash)
-robocopy $VAULT_PATH "$SITE_PATH\content" /E /XD .obsidian .trash /NFL /NDL /NJH /NJS /NC /NS
+Get-ChildItem -LiteralPath $VAULT_PATH -Exclude ".obsidian",".trash" | ForEach-Object {
+    $dest = Join-Path "$SITE_PATH\content" $_.Name
+    if ($_.PSIsContainer) {
+        Copy-Item -LiteralPath $_.FullName -Destination $dest -Recurse -Force
+    } else {
+        Copy-Item -LiteralPath $_.FullName -Destination $dest -Force
+    }
+}
 
 Set-Location "$SITE_PATH"
 
