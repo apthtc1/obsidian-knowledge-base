d87b7d25896b40c79693b45ec02fcf28d7f2d89c
apthtc1 <apthtc78@gmail.com>
2026-07-25 18:15:22 +0300

    Update knowledge base 2026-07-25 18:15

--- publish.ps1
+++ publish.ps1
@@ -5,7 +5,11 @@ $SITE_PATH = "$env:USERPROFILE\quartz-site"
 
 Write-Host "Syncing notes from Obsidian..."
 
-robocopy "$VAULT_PATH" "$SITE_PATH\content" /E /XD .obsidian .trash /NFL /NDL /NJH /NJS /NC /NS
+# Очищаем content перед копированием
+Remove-Item -Path "$SITE_PATH\content\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+# Копируем все файлы и папки из vault (кроме .obsidian и .trash)
+robocopy $VAULT_PATH "$SITE_PATH\content" /E /XD .obsidian .trash /NFL /NDL /NJH /NJS /NC /NS
 
 Set-Location "$SITE_PATH"
 
